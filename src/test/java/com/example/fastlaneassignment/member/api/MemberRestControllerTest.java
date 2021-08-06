0044cbd4f6d69d68c8f38064beab9cb6f4f6ac3f
Gumin Jeong <jgm0710@gmail.com>
2021-08-06 15:12:06 +0900

    Feat - WebMvcTest 설정 및 Rest docs 설정 완료
    - Test 용 Spring Security 설정 구현
    - 일반 Spring Security 설정은 테스트 시 돌아가지 않도록 설정

--- src/test/java/com/example/fastlaneassignment/member/api/MemberRestControllerTest.java
+++ src/test/java/com/example/fastlaneassignment/member/api/MemberRestControllerTest.java
@@ -4,17 +4,13 @@ import com.example.fastlaneassignment.auth.service.AuthApplicationService;
 import com.example.fastlaneassignment.common.BaseWebMvcTest;
 import com.example.fastlaneassignment.member.api.dto.MemberCreateRequestDto;
 import com.example.fastlaneassignment.member.service.MemberApplicationService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,16 +18,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 class MemberRestControllerTest extends BaseWebMvcTest {
 
-    @Autowired
-    MockMvc mockMvc;
-
     @MockBean
     MemberApplicationService memberApplicationService;
 
     @MockBean
     AuthApplicationService authApplicationService;
-
-    ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     @DisplayName("회원 가입")
@@ -48,7 +39,11 @@ class MemberRestControllerTest extends BaseWebMvcTest {
 
         //then
         perform
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andDo(document(
+                        "sign-up-success"
+                ))
+        ;
 
     }
 
