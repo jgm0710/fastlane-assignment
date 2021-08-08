b191ea14b9cf7867963dd9d7153863d1ff73ca60
Gumin Jeong <jgm0710@gmail.com>
2021-08-08 14:06:46 +0900

    Modify - 회원, 인증 문서 보완

--- src/main/java/com/example/fastlaneassignment/common/ResponseCode.java
+++ src/main/java/com/example/fastlaneassignment/common/ResponseCode.java
@@ -8,7 +8,7 @@ public enum ResponseCode implements EnumType{
     /**
      * authentication
      */
-    PASSWORD_MISMATCH("AT001", "비밀번호가 일치하지 않습니다.", "기존 회원의 비밀번호와 일치하는지 확인되는 비밀번호가 일치하지 않을 경우"),
+    PASSWORD_MISMATCH("AT001", "비밀번호가 틀렸습니다.", "비밀번호가 틀렸을 경우"),
     MANIPULATE_OTHER_MEMBERS_RESOURCE("AT002", "다른 회원의 리소스는 조작할 수 없습니다.", "다른 회원의 리소스를 조작하는 경우"),
     REFRESH_TOKEN_NOT_VALID("AT003", "Refresh Token 이 유효하지 않습니다.","인증 토큰 재발급에 필요한 Refresh Token 이 유효하지 않은 경우"),
 
@@ -22,7 +22,7 @@ public enum ResponseCode implements EnumType{
     /**
      * common
      */
-    UNAUTHORIZED("CM001", "인증되지 않은 사용자 입니다.", "Access Token 을 통한 인증이 통과되지 않은 회원인 경우"),
+    UNAUTHORIZED("CM001", "인증되지 않은 사용자 입니다.", "Access Token 을 통해 인증되지 않은 회원인 경우"),
     FORBIDDEN("CM002", "해당 리소스에 접근 권한이 없는 사용자 입니다.", "인증은 되었으나, 리소스에 접근할 권한이 없는 회원인 경우"),
     NOT_VALID("CM003", "요청 시 기입 사항이 올바르게 기입되지 않았습니다.", "API 요청 시 Parameter 나 RequestBody 를 통해 들어오는 값이 지정한 규칙에 맞지 않는 경우"),
     ;
